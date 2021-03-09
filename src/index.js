9ad3f41606f4514da539bf35d8cfefe8019feaf7
Stefany oliveira <desenvolvedoraweb@gmail.com>
2021-03-09 18:29:40 -0300

    Incluindo img, font , xml e csv

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './preta.png';
+import Data from './lista.xml';
+import Notes from './lista.csv';
 
 function component() {
     const element = document.createElement('div');
+    
     element.classList.add("hello");
-  
-    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
+    // Adicione a imagem ao nosso div existente
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    // Exibir arquivos xms e csv no console
+    console.log(Data);
+    console.log(Notes);
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,36 @@ module.exports = {
     module: {
         rules: [
             {
+            // JS
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
             },
             {
+            // CSS
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+            //Imagens 
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+            // fontes
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            },
+            {
+            // arquivos CSV/TSV
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+            //   Arquivos XML
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+              },
         ]
     },
     plugins: [
